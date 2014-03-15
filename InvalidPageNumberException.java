20032a1b2af181d5211ba0575e072c439f9b5b3d
ahmedaboemera <ahmedaboemera@users.noreply.github.com>
2014-03-15 17:31:15 +0200

    Create InvalidPageNumberException.java

---
+++ InvalidPageNumberException.java
@@ -0,0 +1,14 @@
+package diskmgr;
+import chainexception.*;
+
+public class InvalidPageNumberException extends ChainException {
+  
+  
+  public InvalidPageNumberException(Exception ex, String name) 
+    { 
+      super(ex, name); 
+    }
+}
+
+
+
